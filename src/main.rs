c7a10442101a3dd950702548cfb3d0b4f3c67d47
Richard Liebig <liebig.richard@hotmail.com>
2021-01-28 22:04:43 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+include!("single.rs");
+include!("grid.rs");
+extern crate minifb;
+extern crate image;
+
+use minifb::{Key, Window, WindowOptions};
+use std::path::Path;
+use std::fs::File;
+
+
+fn main() {
+    grid();
+}
